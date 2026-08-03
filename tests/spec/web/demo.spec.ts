2f68d45ae6905ba66a822b63db5224588dbed2cc
Thinh Dao <daonguyenthinh96@gmail.com>
2026-08-03 19:41:44 +0700

    add test step

--- tests/spec/web/demo.spec.ts
+++ tests/spec/web/demo.spec.ts
@@ -9,25 +9,42 @@ test.describe('Demoblaze web flows', () => {
   });
 
   test('Complete purchase flow', {tag: ['@cart', '@regression']}, async ({ loginPage, cartPage }) => {
-    await loginPage.addItemsToCart(testData.itemsToAdd);
-    await loginPage.navigateTab('Cart');
-    await cartPage.verifyItemsInCart(testData.itemsToAdd);
-    await cartPage.verifyTotalPriceInCart(testData.itemsToAdd);
-    await cartPage.clickPlaceOrderBtn();
-    await cartPage.inputInformationInPlaceOrder({
-      name: testData.nameInPlaceOrder,
-      country: testData.countryInPlaceOrder,
-      city: testData.cityInPlaceOrder,
-      creditCard: testData.creditCardInPlaceOrder,
-      month: testData.monthInPlaceOrder,
-      year: testData.yearInPlaceOrder,
+    await test.step('Add items to cart', async () => {
+      await loginPage.addItemsToCart(testData.itemsToAdd);
     });
-    await cartPage.clickPurchaseBtn();
-    await cartPage.verifyPurchaseSuccessfully(
-      testData.creditCardInPlaceOrder,
-      testData.nameInPlaceOrder,
-      testData.yearInPlaceOrder,
-    );
-    await cartPage.clickOKBtn();
+
+    await test.step('Navigate to Cart tab', async () => {
+      await loginPage.navigateTab('Cart');
+    });
+
+    await test.step('Verify items and total price in cart', async () => {
+      await cartPage.verifyItemsInCart(testData.itemsToAdd);
+      await cartPage.verifyTotalPriceInCart(testData.itemsToAdd);
+    });
+
+    await test.step('Place order and input information', async () => {
+      await cartPage.clickPlaceOrderBtn();
+      await cartPage.inputInformationInPlaceOrder({
+        name: testData.nameInPlaceOrder,
+        country: testData.countryInPlaceOrder,
+        city: testData.cityInPlaceOrder,
+        creditCard: testData.creditCardInPlaceOrder,
+        month: testData.monthInPlaceOrder,
+        year: testData.yearInPlaceOrder,
+      });
+    });
+
+    await test.step('Complete purchase', async () => {
+      await cartPage.clickPurchaseBtn();
+    });
+
+    await test.step('Verify purchase successfully and close confirmation', async () => {
+      await cartPage.verifyPurchaseSuccessfully(
+        testData.creditCardInPlaceOrder,
+        testData.nameInPlaceOrder,
+        testData.yearInPlaceOrder,
+      );
+      await cartPage.clickOKBtn();
+    });
   });
 });
